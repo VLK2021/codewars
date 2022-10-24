55df4404a6cc94c03d44f138da57e32c28d57d20
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-10-24 23:53:44 +0300

    next

--- codeWars.js
+++ codeWars.js
@@ -6,4 +6,16 @@
 // let w = makeUpperCase('wwwww');
 // console.log(w);
 
+
+//The goal is to create a function 'numberToPower(number, power)' that "raises"
+// the number up to power (ie multiplies number by itself power times).
+
+// function numberToPower(number, power) {
+//     let result = 1;
 //
+//     for (let i = 1; i <= power; i++) {
+//         result *= number;
+//     }
+//     return result;
+// }
+// console.log(numberToPower(10, 6));
