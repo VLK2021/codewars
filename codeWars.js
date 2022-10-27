b8ea466edacbb38cbbbc2496e4af211c352e8d98
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-10-28 00:35:01 +0300

    7

--- codeWars.js
+++ codeWars.js
@@ -70,4 +70,33 @@
 // }
 // console.log(positiveSum([1,-4,7,12]));
 
-//-------------------------------------------------------------------------------------------------------------
+//-------------7------------------------------------------------------------------------------------------------
+//Return an array, where the first element is the count of positives numbers and the second element
+// is sum of negative numbers.0 is neither positive nor negative.
+// If the input is an empty array or is null, return an empty array.
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// function countPositivesSumNegatives(input) {
+//     let arr = [];
+//     let countPositive = 0;
+//     let sumNegative = 0;
+//     for (const element of input) {
+//         if (element > 0) {
+//             countPositive = countPositive + 1;
+//         }
+//         if (element < 0) {
+//             sumNegative = sumNegative + element;
+//         }
+//     }
+//     arr.push(countPositive, sumNegative);
+//     return arr;
+// }
+//
+// console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]));
+
+//--------------------------------------------------------------------------------------------------
+
+
+
+
